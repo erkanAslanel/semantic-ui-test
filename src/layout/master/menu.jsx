14b305c20fbbccb81ea2b6a8e47235d0c43d66a2
Erkan Aslanel <erkan.aslanel@gmail.com>
2017-05-06 15:24:32 +0300

    menu active ok

--- src/layout/master/menu.jsx
+++ src/layout/master/menu.jsx
@@ -1,24 +1,38 @@
-import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
-
- class MenuExampleBasic extends Component {
+import React, {Component} from 'react'
+import {Menu} from 'semantic-ui-react'
 
 
-  render() {
+class MenuExampleBasic extends Component {
 
-let activeItem={}
+    constructor() {
+
+        super();
+
+       this.state = {};
+
+         this.handleItemClick = (e, { name }) => {
+
+
+           this.setState({ activeItem: name })
+       };
+    }
+
+
+
+render() {
+
+const { activeItem } = this.state
 
     return (
 
-      <Menu inverted vertical floated fixed='left'>
-          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
-          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+        <Menu inverted vertical floated fixed='left'>
+            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
+            <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick}/>
+            <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick}/>
         </Menu>
 
-
     )
-  }
+}
 }
 
 export default MenuExampleBasic;
